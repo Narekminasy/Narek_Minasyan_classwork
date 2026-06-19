31ce8edb7ff3887c089c216ee8ad2dd700e40d0e
Narek Minasyan <narekminasyan52@gmail.com>
2026-06-19 19:40:13 +0400

    add app.js and index.js routes  logic

---
+++ app.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import 'dotenv/config';
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.send('First');
+});
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
